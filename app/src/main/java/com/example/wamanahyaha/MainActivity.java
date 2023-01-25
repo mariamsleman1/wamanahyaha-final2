d29ec691813c239ee3e127a502bc0c5899ce5d1d
mariamsleman1 <mariamsleman07@gmail.com>
2023-01-26 00:33:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/wamanahyaha/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.wamanahyaha;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayout,new thefaceoftheappfragment());
+        ft.commit();
+    }
+}
